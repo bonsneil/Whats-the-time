6a5cf80b7e6c5b3b3fea051822c55fa06e1ceccd
bonsneil <Neil.Hathaway@novasystems.com>
2017-06-28 18:00:42 +1000

    GET for contact messages json

---
+++ js/msgBrowser.js
@@ -0,0 +1,13 @@
+﻿
+$(document).ready(function () {
+    // when the document is ready go and get the messages
+    console.log("ready");
+    $.getJSON('messages.json', function (msgJSON) {
+        console.log(msgJSON);
+    });
+
+    $.getJSON('contactDatabase.json', function (cMsgJSON) {
+        console.log(cMsgJSON);
+    });
+
+});
